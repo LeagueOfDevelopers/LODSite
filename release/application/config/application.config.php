29b0bd517229b9a69aeebcb6e6ef9ff7c6b59aaa
Anton Sokolov <profox.rus@gmail.com>
2015-03-02 01:56:03 +0300

    Update basic templates

--- release/application/config/application.config.php
+++ release/application/config/application.config.php
@@ -31,8 +31,9 @@ return array(
         'common_includes' => array(
             'title' => 'Лига Разработчиков',
             'meta' => array(
-                'charset' => '<meta http-equiv="content-type" content="text/html; charset=utf-8">',
+                'charset' => '<meta charset="utf-8">',
                 'ie' => '<meta http-equiv="X-UA-Compatible" content="IE=edge">',
+                'viewport' => '<meta name="viewport" content="width=device-width, initial-scale=1">',
                 'yandex.metrika' => '<script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter26889786 = new Ya.Metrika({id:26889786, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="//mc.yandex.ru/watch/26889786" style="position:absolute; left:-9999px;" alt="" /></div></noscript>'
             ),
             'script' => array(
